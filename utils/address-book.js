7bb449f550a833fbaed1d54ae98f36b68b6fd438
chou0122 <74644793+chou0122@users.noreply.github.com>
2023-11-20 11:53:53 +0800

    11-20 12:00

--- utils/address-book.js
+++ utils/address-book.js
@@ -3,16 +3,29 @@ import db from "./../utils/connect-mysql.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+const getListData = async (req) => {
   const perPage = 20; // 每頁幾筆
   let page = +req.query.page || 1;
-  let totalRows;
-  let totalPages;
+  let totalRows = 0;
+  let totalPages = 0;
   let rows = [];
 
+  let output = {
+    success: false,
+    page,
+    perPage,
+    rows,
+    totalRows,
+    totalPages,
+
+    redirect: "",
+    info: "",
+  };
+
   if (page < 1) {
-    // return res.redirect(req.baseUrl);
-    return res.redirect(`?page=1`);
+    output.redirect = `?page=1`;
+    output.info = `頁碼值小於 1`;
+    return output;
   }
 
   const t_sql = "SELECT COUNT(1) totalRows FROM address_book";
@@ -20,20 +33,32 @@ router.get("/", async (req, res) => {
   totalPages = Math.ceil(totalRows / perPage);
   if (totalRows > 0) {
     if (page > totalPages) {
-      return res.redirect(`?page=${totalPages}`);
+      output.redirect = `?page=${totalPages}`;
+      output.info = `頁碼值大於總頁數`;
+      return { ...output, totalRows, totalPages };
     }
 
     const sql = `SELECT * FROM address_book ORDER BY sid DESC 
     LIMIT ${(page - 1) * perPage}, ${perPage}`;
     [rows] = await db.query(sql);
+    output = { ...output, success: true, rows, totalRows, totalPages };
   }
 
-  res.render('address-book/list', {
-    page,
-    totalRows,
-    totalPages,
-    rows,
-  });
+  return output;
+};
+
+router.get("/", async (req, res) => {
+  res.locals.pageName = "ab-list";
+  const output = await getListData(req);
+  if (output.redirect) {
+    return res.redirect(output.redirect);
+  }
+
+  res.render("address-book/list", output);
+});
+
+router.get("/api", async (req, res) => {
+  res.json(await getListData(req));
 });
 
 export default router;
