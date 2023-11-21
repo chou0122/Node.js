17afe3fd3a91dfc0b95e4b4ce459fb6f82b6d221
chou0122 <74644793+chou0122@users.noreply.github.com>
2023-11-21 09:34:27 +0800

    Create address-book.js

---
+++ ruotes/address-book.js
@@ -0,0 +1,89 @@
+import express from "express";
+import db from "./../utils/connect-mysql.js";
+import upload from "./../utils/upload-imgs.js";
+
+const router = express.Router();
+
+const getListData = async (req) => {
+  const perPage = 20; // 每頁幾筆
+  let page = +req.query.page || 1;
+  let totalRows = 0;
+  let totalPages = 0;
+  let rows = [];
+
+  let output = {
+    success: false,
+    page,
+    perPage,
+    rows,
+    totalRows,
+    totalPages,
+
+    redirect: "",
+    info: "",
+  };
+
+  if (page < 1) {
+    output.redirect = `?page=1`;
+    output.info = `頁碼值小於 1`;
+    return output;
+  }
+
+  const t_sql = "SELECT COUNT(1) totalRows FROM address_book";
+  [[{ totalRows }]] = await db.query(t_sql);
+  totalPages = Math.ceil(totalRows / perPage);
+  if (totalRows > 0) {
+    if (page > totalPages) {
+      output.redirect = `?page=${totalPages}`;
+      output.info = `頁碼值大於總頁數`;
+      return { ...output, totalRows, totalPages };
+    }
+
+    const sql = `SELECT * FROM address_book ORDER BY sid DESC 
+    LIMIT ${(page - 1) * perPage}, ${perPage}`;
+    [rows] = await db.query(sql);
+    output = { ...output, success: true, rows, totalRows, totalPages };
+  }
+
+  return output;
+};
+
+router.get("/", async (req, res) => {
+  res.locals.pageName = "ab-list";
+  const output = await getListData(req);
+  if (output.redirect) {
+    return res.redirect(output.redirect);
+  }
+
+  res.render("address-book/list", output);
+});
+
+router.get("/api", async (req, res) => {
+  res.json(await getListData(req));
+});
+
+router.get("/add", async (req, res) => {
+  res.render("address-book/add");
+});
+router.post("/add", upload.none(), async (req, res) => {
+
+  const {name, email, mobile, birthday, address} = req.body;
+  const sql = "INSERT INTO `address_book`(`name`, `email`, `mobile`, `birthday`, `address`, `created_at`) VALUES (?, ?, ?, ?, ?, NOW() )";
+
+  const [result] = await db.query(sql, [name, email, mobile, birthday, address]);
+
+  /*
+  {
+    "fieldCount": 0,
+    "affectedRows": 1,  # 影響的列數
+    "insertId": 1021,   # 取得的 PK 
+    "info": "",
+    "serverStatus": 2,
+    "warningStatus": 0,
+    "changedRows": 0    # 修改時真正有變動的資料筆數
+  }
+  */
+  res.json(result);
+});
+
+export default router;
